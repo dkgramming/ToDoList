a30e491fd5ccb97a6c77947748c6b4982d3863ef
David Kisluk <david.kisluk@gmail.com>
2015-01-30 19:44:34 -0500

    Creates a subclass of UITableViewController for the to-do-list table view

---
+++ ToDoList/ToDoListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ToDoListTableViewController.h
+//  ToDoList
+//
+//  Created by Student on 1/30/15.
+//  Copyright (c) 2015 David Kisluk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToDoListTableViewController : UITableViewController
+
+@end
